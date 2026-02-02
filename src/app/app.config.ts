51a7fd7f64e81399feaa7e29fde6f00b9e7f4327
sarobidy-23 <hei.sarobidy@gmail.com>
2026-02-02 07:02:33 +0300

    config: use alias import typescript

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import {
 } from '@angular/core';
 import {provideRouter} from '@angular/router';
 
-import {routes} from './app.routes';
+import {routes} from '@/app';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
+import {App, appConfig} from '@/app';
 import {bootstrapApplication} from '@angular/platform-browser';
-import {App} from './app/app';
-import {appConfig} from './app/app.config';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
