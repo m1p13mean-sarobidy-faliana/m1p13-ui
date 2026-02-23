31c6cd770b68ec74d3108a44ce3b1efef6b7dd34
sarobidy-23 <hei.sarobidy@gmail.com>
2026-02-23 07:53:09 +0300

    feat: add toast notification

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,11 +8,18 @@ import {
   withEventReplay,
 } from '@angular/platform-browser';
 import {provideRouter} from '@angular/router';
+import {provideToastr} from 'ngx-toastr';
+import 'ngx-toastr/toastr';
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      closeButton: true,
+    }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
   ],
