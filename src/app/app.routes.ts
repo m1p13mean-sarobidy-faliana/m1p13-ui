e78dc120138f5cc4b14652a7ab50aca7f4a335b3
sarobidy-23 <hei.sarobidy@gmail.com>
2026-02-25 12:29:23 +0300

    feat: verify-email pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
-import {ConfirmPassword, Login, NotFound} from '@/app/pages';
+import {
+  ConfirmPassword,
+  Login,
+  NotFound,
+  VerifyEmail,
+} from '@/app/pages';
 import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   {path: 'login', component: Login},
   {path: 'password/:token', component: ConfirmPassword},
+  {path: 'verify-email/:token', component: VerifyEmail},
   {path: '**', component: NotFound},
 ];
--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,3 +1,4 @@
 export * from './confirm-password/confirm-password';
 export * from './login/login';
 export * from './not-found/not-found';
+export * from './verify-email/verify-email';
