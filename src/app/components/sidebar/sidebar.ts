25464d7a57d485cd556c5643251734fde4834aed
sarobidy-23 <hei.sarobidy@gmail.com>
2026-03-02 22:04:51 +0300

    fix(sidebar): navigate with logged/unlogged user sidebar menu

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -21,33 +21,6 @@ export class Sidebar {
   readonly loggedUser = computed(() => this.authProvider.currentUser());
   readonly currentUrl = computed(() => this.router.url);
 
-  possibleRoutes = [
-    {
-      url: '/dashboard',
-      label: 'Tableau de bord',
-      authorized: this.authProvider.isAdmin(),
-      icon: 'pi pi-clipboard',
-    },
-    {
-      url: '/catalogs',
-      label: 'Catalogue',
-      authorized: true,
-      icon: 'pi pi-objects-column',
-    },
-    {
-      url: '/shops',
-      label: 'Boutique',
-      authorized: true,
-      icon: 'pi pi-shop',
-    },
-    {
-      url: '/cart',
-      label: 'Panier',
-      authorized: true,
-      icon: 'pi pi-shopping-cart',
-    },
-  ];
-
   navigate(route: string) {
     this.router.navigate([route]);
   }
