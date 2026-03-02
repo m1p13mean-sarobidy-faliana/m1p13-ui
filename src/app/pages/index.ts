9340df1d4bad96678cbb2a9641e6c5e81a9bdaef
sarobidy-23 <hei.sarobidy@gmail.com>
2026-03-02 22:05:33 +0300

    fix(import): profile import from index.ts

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -4,6 +4,7 @@ export * from './catalog/catalog';
 export * from './confirm-password/confirm-password';
 export * from './login/login';
 export * from './not-found/not-found';
+export * from './profile/profile';
 export * from './register/register';
 export * from './shop/shop';
 export * from './verify-email/verify-email';
