a429bb163d79bd8769681aa6649bf70865c4dc40
sarobidy-23 <hei.sarobidy@gmail.com>
2026-03-02 11:32:33 +0300

    fix(sidebar): sidebar is different for mobile & desktop

--- src/app/components/sidebar/sidebar-content.ts
+++ src/app/components/sidebar/sidebar-content.ts
@@ -1,21 +1,18 @@
 import {AuthProvider} from '@/app/providers/auth-provider';
-import {Screen} from '@/app/utils/screen';
 import {SidebarService} from '@/app/utils/sidebar';
 import {Component, computed, inject} from '@angular/core';
 import {Router} from '@angular/router';
 import {AvatarModule} from 'primeng/avatar';
 import {ButtonModule} from 'primeng/button';
 import {DrawerModule} from 'primeng/drawer';
-import {SidebarContent} from './sidebar-content';
 
 @Component({
-  selector: 'sidebar',
-  imports: [AvatarModule, DrawerModule, ButtonModule, SidebarContent],
-  templateUrl: './sidebar.html',
+  selector: 'sidebar-content',
+  imports: [AvatarModule, DrawerModule, ButtonModule],
+  templateUrl: './sidebar-content.html',
 })
-export class Sidebar {
+export class SidebarContent {
   private router = inject(Router);
-  screen = inject(Screen);
   sidebarService = inject(SidebarService);
   authProvider = inject(AuthProvider);
   readonly loggedUser = computed(() => this.authProvider.currentUser());
